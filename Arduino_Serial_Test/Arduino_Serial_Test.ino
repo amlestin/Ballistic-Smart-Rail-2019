8c34fd0cf471969e652c87f7c277368b3e64acae
Jesse <jess.t.moody@gmail.com>
2019-06-25 16:59:40 -0400

    Serial "Splitter"
    Now allows you to send serial message via PC serial monitor which then sends to Pi's GPIO serial port (AMA0).

--- Arduino_Serial_Test/Arduino_Serial_Test.ino
+++ Arduino_Serial_Test/Arduino_Serial_Test.ino
@@ -1,11 +1,14 @@
 void setup() {
-  Serial.begin(115200);  // Pi
+Serial.begin(115200); //PC serial monitor
+Serial2.begin(115200); //start connection to Pi's GPIO serial port
 }
 
 void loop() {
-  Serial.println("Testing 123");
-  digitalWrite(13, HIGH);
-  delay(600);
-  digitalWrite(13, LOW);
-  delay(600);
+if (Serial.available() > 0) { //read from PC serial monitor
+int incoming = Serial.read();
+Serial.print("character recieved: "); //output to PC serial monitor...
+Serial2.print("character recieved: "); //then output to Pi
+Serial.println(incoming, DEC);
+Serial2.println(incoming, DEC);
+}
 }
